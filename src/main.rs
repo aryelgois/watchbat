a25c14e9d219d23259f52d33be8efe984ae43b60
Aryel Mota Góis <aryel.gois@gmail.com>
2022-11-16 19:04:39 -0300

    Implement `Default` for `Config`

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ mod battery;
 mod error;
 mod validate;
 
+use std::time::Duration;
+
+use battery::percentage::Breakpoints;
+use battery::watcher::{Config, Watcher};
+
+impl Default for Config {
+    fn default() -> Self {
+        Config::new(
+            String::from("/sys/class/power_supply/BAT0/capacity"),
+            Breakpoints::new(10, 13, 94, 97).unwrap(),
+            Duration::from_secs(45),
+        )
+        .unwrap()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let watcher = Watcher::new(Config::default());
 }
